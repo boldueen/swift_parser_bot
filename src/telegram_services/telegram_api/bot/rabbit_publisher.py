aa53cdd6194d3c5280ea6f1d24c29ede0f94d1d0
boldueen <nikon2283@gmail.com>
2023-08-26 16:11:46 +0300

    [telegram_api] (fix) - delete useless consumer code

--- src/telegram_services/telegram_api/bot/rabbit_publisher.py
+++ src/telegram_services/telegram_api/bot/rabbit_publisher.py
@@ -9,18 +9,6 @@ from pika.exchange_type import ExchangeType
 logger = logging.getLogger(__name__)
 
 
-class Consumer():
-    def __init__(self) -> None:
-        connection_string = ConnectionParameters(
-            host='rabbitmq',
-            heartbeat=600,
-            blocked_connection_timeout=300
-        )
-
-
-logger = logging.getLogger(__name__)
-
-
 class Publisher():
     def __init__(self) -> None:
         connection_params = ConnectionParameters(
