28f65104dcd9e5b401251b91d487704810750651
boldueen <nikon2283@gmail.com>
2023-08-27 18:59:58 +0300

    [telegram-api] (feature) - add commands for yandex_b2c and citymobil

--- src/telegram_services/telegram_api/bot/handlers/tariffs.py
+++ src/telegram_services/telegram_api/bot/handlers/tariffs.py
@@ -24,9 +24,21 @@ async def send_yandex_b2b(message: Message):
 
 @router.message(Command('yandex_b2c'))
 async def send_yandex_b2c(message: Message):
-    pass
+    await message.reply('wait a second...')
+    payload = {
+        'filetype': 'yandex_b2c',
+        'id_to_send_file_to': message.from_user.id
+    }
+
+    await publisher.publish('file_sender', payload)
 
 
 @router.message(Command('citymobil'))
-async def send_yandex_citymobil(message: Message):
-    pass
+async def send_citymobil(message: Message):
+    await message.reply('wait a second...')
+    payload = {
+        'filetype': 'citymobil',
+        'id_to_send_file_to': message.from_user.id
+    }
+
+    await publisher.publish('file_sender', payload)
